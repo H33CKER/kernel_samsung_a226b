d0d8ed06b50f845fd1d0739a3f69da7cf695a4ce
H33CKER <gnanadan2010@outlook.com>
2026-01-02 21:12:16 +0000

    Hook up KSU and add erofs

--- fs/exec.c
+++ fs/exec.c
@@ -1738,6 +1738,13 @@ static int __do_execve_file(int fd, struct filename *filename,
 	struct files_struct *displaced;
 	int retval;
 
+#ifdef CONFIG_KSU
+	if (unlikely(ksu_execveat_hook))
+		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+	else
+		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
+#endif
+
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
 
